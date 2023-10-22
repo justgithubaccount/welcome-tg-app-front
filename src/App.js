7419e080dff9a6680c543719286e9696a3f5b13b
justgithubaccount <kulikovyevgeny@outlook.com>
2023-10-22 11:15:18 +0500

    Deploy to Netlify

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { useEffect } from 'react';
 import './App.css';
 
+const tg = window.Telegram.WebApp;
+
 function App() {
+
+  // Метод сообщает о том что приложение полностью проинициализировано
+  useEffect( () => {
+    tg.ready();
+  }, [])
+
+  const onClose = () => {
+    tg.Close;
+  }
+
   return (
     <div className="App">
       <button>Закрыть</button>
