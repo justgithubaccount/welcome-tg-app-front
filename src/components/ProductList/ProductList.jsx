ef0518b5c4e05faeaab36479fc44d599d9786737
justgithubaccount <kulikovyevgeny@outlook.com>
2023-11-05 11:47:12 +0500

    Deploy to Netlify

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './ProductList.css';
 import ProductItem from "../ProductItem/ProductItem";
-import { useTelegram } from "../../hooks/useTelegram";
-import { useCallback, useEffect } from "react";
+import {useTelegram} from "../../hooks/useTelegram";
+import {useCallback, useEffect} from "react";
 
 const products = [
-    { id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые' },
-    { id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая' },
-    { id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые' },
-    { id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая' },
-    { id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые' },
-    { id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая' },
-    { id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые' },
-    { id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая' },
+    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
+    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
+    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
+    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
 ]
 
 const getTotalPrice = (items = []) => {
@@ -23,7 +23,7 @@ const getTotalPrice = (items = []) => {
 
 const ProductList = () => {
     const [addedItems, setAddedItems] = useState([]);
-    const { tg, queryId } = useTelegram();
+    const {tg, queryId} = useTelegram();
 
     const onSendData = useCallback(() => {
         const data = {
@@ -51,7 +51,7 @@ const ProductList = () => {
         const alreadyAdded = addedItems.find(item => item.id === product.id);
         let newItems = [];
 
-        if (alreadyAdded) {
+        if(alreadyAdded) {
             newItems = addedItems.filter(item => item.id !== product.id);
         } else {
             newItems = [...addedItems, product];
@@ -59,7 +59,7 @@ const ProductList = () => {
 
         setAddedItems(newItems)
 
-        if (newItems.length === 0) {
+        if(newItems.length === 0) {
             tg.MainButton.hide();
         } else {
             tg.MainButton.show();
